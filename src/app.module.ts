08f0405ad9b14a88278c2754120ff37b654d011f
Cristian Gonzalez <cgonsevi@gmail.com>
2022-06-09 15:28:35 -0600

    feat: :sparkles:  Import database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './config/database/database.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
