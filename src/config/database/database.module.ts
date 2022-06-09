54c1ff96ff1e4f83afa312167b09d5db6e6e572d
Cristian Gonzalez <cgonsevi@gmail.com>
2022-06-09 15:28:19 -0600

    feat: :sparkles: Add database module

---
+++ src/config/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'admin',
+      password: 'sa384719',
+      database: 'chat',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
