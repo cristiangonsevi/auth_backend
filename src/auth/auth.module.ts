dc83c07d6b6d97343c5b30a86133ba2df87d5f51
Cristian Gonzalez <cgonsevi@gmail.com>
2022-06-08 19:02:08 -0600

    feat: :sparkles: Import login files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RegisterController } from './register/register.controller';
 import { RegisterService } from './register/register.service';
+import { LoginController } from './login/login.controller';
+import { LoginService } from './login/login.service';
 
 @Module({
-  controllers: [RegisterController],
-  providers: [RegisterService],
+  controllers: [RegisterController, LoginController],
+  providers: [RegisterService, LoginService],
 })
 export class AuthModule {}
