fb0e26394cee60322334bd7672e6da1b23ee0d8c
Alberto <cgonsevi@gmail.com>
2022-06-09 19:57:33 -0600

    feat: :sparkles: Add service and method for register a new user

--- src/auth/register/register.controller.ts
+++ src/auth/register/register.controller.ts
@@ -1,12 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { RegisterUserDto } from '../dto';
+import { SignInType } from '../enums/signInType';
 import { RegisterService } from './register.service';
 
 @Controller('register')
 export class RegisterController {
   constructor(private readonly _registerService: RegisterService) {}
   @Post()
-  register(@Body() dto: RegisterUserDto): string {
-    return this._registerService.register(dto);
+  async register(
+    @Query('auth') authMetod: SignInType = SignInType.EMAIL,
+    @Body() dto: RegisterUserDto,
+  ): Promise<any> {
+    if (!SignInType[authMetod.toUpperCase()]) {
+      throw new BadRequestException('Invalid auth method');
+    }
+    dto.authMethod = authMetod;
+    return await this._registerService.register(dto);
   }
 }
--- src/auth/register/register.service.ts
+++ src/auth/register/register.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RegisterUserDto } from '../dto';
+import { User } from '../entity/user.entity';
 
 @Injectable()
 export class RegisterService {
-  register(dto): string {
-    return dto;
+  logger: Logger = new Logger('RegisterService');
+  constructor(
+    @InjectRepository(User)
+    private _userRepository: Repository<User>,
+  ) {}
+  async register(dto: RegisterUserDto): Promise<User> {
+    try {
+      return await this._userRepository.save(dto);
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException('Error registering user');
+    }
   }
 }
