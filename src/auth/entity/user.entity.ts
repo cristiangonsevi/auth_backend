a6af6981847d0a13a8cb4afb31823f42684943bf
Alberto <cgonsevi@gmail.com>
2022-06-09 19:55:46 -0600

    feat: :sparkles: Add user entity

---
+++ src/auth/entity/user.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { SignInType } from '../enums/signInType';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({ length: 60 })
+  firstName: string;
+  @Column({ length: 60, nullable: true })
+  lastName: string;
+  @Column({ length: 100, unique: true })
+  email: string;
+  @Column()
+  password: string;
+  @Column({ nullable: true })
+  image: string;
+  @Column({ length: 20, default: SignInType.EMAIL })
+  authMethod: string;
+  @Column({ default: true })
+  isActive: boolean;
+  @CreateDateColumn()
+  createdAt: Date;
+}
