f110a08daeb9b7e52750694567ef3ef9cae74eeb
Alberto <cgonsevi@gmail.com>
2022-06-09 19:56:27 -0600

    feat: :sparkles: Add entity to module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,11 @@ import { RegisterController } from './register/register.controller';
 import { RegisterService } from './register/register.service';
 import { LoginController } from './login/login.controller';
 import { LoginService } from './login/login.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [RegisterController, LoginController],
   providers: [RegisterService, LoginService],
 })
