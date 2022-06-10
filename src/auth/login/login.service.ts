9a7a9248302a655602e3476cf021053af7633699
Alberto <cgonsevi@gmail.com>
2022-06-09 19:57:08 -0600

    feat: :sparkles: Add service for login

--- src/auth/login/login.service.ts
+++ src/auth/login/login.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { LoginUserDto } from '../dto/login.dto';
+import { User } from '../entity/user.entity';
 
 @Injectable()
 export class LoginService {
-  login(dto: LoginUserDto): any {
-    return dto;
+  constructor(
+    @InjectRepository(User)
+    private _userRepository: Repository<User>,
+  ) {}
+
+  async login(dto: LoginUserDto): Promise<User> {
+    try {
+      return await this._userRepository.findOneBy({ email: dto.email });
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
