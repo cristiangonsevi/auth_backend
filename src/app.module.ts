dfdb64f9ff76ff42f669605c76ea3d2eba410b9a
Alberto Sevillano <cgonsevi@gmail.com>
2022-06-10 21:00:50 -0600

    feat: :sparkles: Add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import configuration from './config/configuration';
 import { DatabaseModule } from './config/database/database.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
