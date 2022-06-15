c77a71a323bf742ddef41708c95551deea25eeb2
Alberto Sevillano <cgonsevi@gmail.com>
2022-06-14 21:28:24 -0600

    feat: :sparkles: Import googleService and configService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,12 @@ import { LoginController } from './login/login.controller';
 import { LoginService } from './login/login.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { GoogleAuthService } from './services/google-auth.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
   controllers: [RegisterController, LoginController],
-  providers: [RegisterService, LoginService],
+  providers: [RegisterService, LoginService, GoogleAuthService],
 })
 export class AuthModule {}
