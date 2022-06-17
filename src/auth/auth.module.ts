4fde80b9dd9c9f9f641d12d959b1f0bb0009a05e
Alberto Sevillano <cgonsevi@gmail.com>
2022-06-17 12:28:58 -0600

    feat: :sparkles: Import githubAuth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,10 +7,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { GoogleAuthService } from './services/google-auth.service';
 import { ConfigModule } from '@nestjs/config';
+import { GithubAuthService } from './services/github-auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), ConfigModule],
   controllers: [RegisterController, LoginController],
-  providers: [RegisterService, LoginService, GoogleAuthService],
+  providers: [
+    RegisterService,
+    LoginService,
+    GoogleAuthService,
+    GithubAuthService,
+  ],
 })
 export class AuthModule {}
