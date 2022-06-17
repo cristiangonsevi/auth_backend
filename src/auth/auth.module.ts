800d06abdbbf9383b56b6d03ca6a481a4371fc6c
Alberto Sevillano <cgonsevi@gmail.com>
2022-06-17 15:48:46 -0600

    feat: :sparkles: Add HttpModule to request and ConfigModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+
 import { RegisterController } from './register/register.controller';
 import { RegisterService } from './register/register.service';
 import { LoginController } from './login/login.controller';
 import { LoginService } from './login/login.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { User } from './entity/user.entity';
+
 import { GoogleAuthService } from './services/google-auth.service';
-import { ConfigModule } from '@nestjs/config';
 import { GithubAuthService } from './services/github-auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
+  imports: [TypeOrmModule.forFeature([User]), ConfigModule, HttpModule],
   controllers: [RegisterController, LoginController],
   providers: [
     RegisterService,
