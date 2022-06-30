4d7eece849cb347450f151135c0758af81d7178a
Alberto Sevillano <cgonsevi@gmail.com>
2022-06-30 16:30:21 -0600

    feat: :sparkles: Create method to upload an image

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,25 @@
 import {
   Body,
   Controller,
+  Get,
   HttpStatus,
   Param,
   ParseIntPipe,
+  Post,
   Put,
+  Query,
+  Res,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UpdateUserDto } from './dto/updateUser.dto';
 import { UserService } from './services/user.service';
 import { hashSync } from 'bcrypt';
 import { UpdateUserPasswordDto } from './dto/updatePassword.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+import { Response } from 'express';
 
 @Controller('user')
 export class UserController {
@@ -36,5 +46,37 @@ export class UserController {
       statusCode: HttpStatus.OK,
       message: 'Password updated succesfully',
     };
+  }
+  @Post(':id/image')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: join(__dirname, '../public/images/users'),
+        filename: (req, file, cb) => {
+          const fileName = `${req.params.id}-user`;
+          cb(null, fileName + extname(file.originalname));
+        },
+      }),
+    }),
+  )
+  async updateImage(
+    @UploadedFile() file: Express.Multer.File,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    await this._userService.updateUserImage(file.filename, id);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Image updated succesfully',
+      file,
+    };
+  }
+  @Get('image')
+  async getFile(@Res() res: Response, @Query('name') image: string) {
+    const isImgExternal = image.search(/http/);
+    if (isImgExternal >= 0) {
+      return res.end(await this._userService.getBase64(image));
+    }
+    const file = join(__dirname, '../public/images/users/' + image);
+    res.sendFile(file);
   }
 }
