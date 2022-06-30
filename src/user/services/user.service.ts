2856cbc827d61a9e5a6c33af8e5b272257be5b28
Alberto Sevillano <cgonsevi@gmail.com>
2022-06-30 12:28:42 -0600

    feat: :sparkles: Create user service

---
+++ src/user/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/auth/entity/user.entity';
+import { UpdateUserDto } from '../dto/updateUser.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private _userRepository: Repository<User>,
+  ) {}
+  async updateUser(dto: UpdateUserDto, id: number) {
+    try {
+      return await this._userRepository.update(id, dto);
+    } catch (error) {
+      console.log(error);
+      new InternalServerErrorException();
+    }
+  }
+}
