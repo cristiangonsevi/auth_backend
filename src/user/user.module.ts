2516ad4043cb35938f9bd7950c2d47fc935ac5a3
Alberto Sevillano <cgonsevi@gmail.com>
2022-06-30 16:31:10 -0600

    feat: :sparkles: Add httpmodule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserController } from './user.controller';
 import { UserService } from './services/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/auth/entity/user.entity';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), HttpModule],
   controllers: [UserController],
   providers: [UserService],
 })
