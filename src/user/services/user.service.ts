41ac1048f8dffaca9c929c4763d6f29bb7b68e42
Alberto Sevillano <cgonsevi@gmail.com>
2022-06-30 16:30:48 -0600

    feat: :sparkles: Add method to download image and convert to base64

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/auth/entity/user.entity';
 import { UpdateUserDto } from '../dto/updateUser.dto';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private _userRepository: Repository<User>,
+    private _http: HttpService,
   ) {}
   async updateUser(dto: UpdateUserDto, id: number) {
     try {
@@ -25,5 +27,23 @@ export class UserService {
       console.log(error);
       new InternalServerErrorException();
     }
+  }
+  async updateUserImage(image: string, id) {
+    try {
+      return await this._userRepository.update(id, { image });
+    } catch (error) {
+      console.log(error);
+      new InternalServerErrorException();
+    }
+  }
+  getBase64(url) {
+    return this._http
+      .get(url, {
+        responseType: 'arraybuffer',
+      })
+      .toPromise()
+      .then((response) => {
+        return Buffer.from(response.data, 'base64');
+      });
   }
 }
