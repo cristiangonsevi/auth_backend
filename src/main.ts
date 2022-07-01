ff37bff2f0d54d6d94b3db942981b508ca09cb8f
Alberto Sevillano <cgonsevi@gmail.com>
2022-07-01 09:35:51 -0600

    feat: :sparkles: Add transform pipe

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3000);
 }
 bootstrap();
