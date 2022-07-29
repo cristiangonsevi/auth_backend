f4631bf59ee784f5b41bbde89f774af60d957970
Alberto Sevillano <cgonsevi@gmail.com>
2022-07-28 19:25:34 -0600

    feat: :sparkles: Add config service

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const ENV = {
+    PORT: configService.get('PORT'),
+  };
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(3000);
+  await app.listen(ENV.PORT);
 }
 bootstrap();
